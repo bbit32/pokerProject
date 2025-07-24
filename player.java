68e867d0c467adc5b8d5e477e6c93bbfa1ff4d87
bbit32 <bennett@thelembkes.net>
2025-07-24 13:12:33 -0500

    Update player.java

--- player.java
+++ player.java
@@ -1,5 +1,50 @@
-package poker;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
 
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
 public class player {
+		private String name;
+		private int balance;
+		private StandardCard[] pockCards = new StandardCard[2];
+		private boolean isInGame;
+	public void Player(String name, int balance, StandardCard[] pockCards)	{
+		this.name = name;
+		this.balance = balance;
+		this.pockCards = pockCards;
+		this.isInGame = true;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public int getBalance() {
+		return this.balance;
+	}
+	public void addToBalance(int additionAmmount) {
+		this.balance+=additionAmmount;
+	}
+	public void subtractToBalance(int reduceAmmount) {
+		this.balance-=reduceAmmount;
+	}
+	public StandardCard[] getPockCards() {
+		return this.pockCards;
+	}
+	public boolean isInGame() {
+		return this.isInGame;
+	}
+	public void setIsInGame(boolean isInGame) {
+		this.isInGame =isInGame;
+	}
+	
+	public String Status() {
+		return ("Player " + this.name + "has a balance of " + this.balance + "and has pocket cards " + this.pockCards[0]+ "and " +this.pockCards[1] + "In game:" + this.isInGame);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
 
 }
