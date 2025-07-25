f67f7eb7b58c302aeff1a726616d7b21e772a744
R2MC <ryanmmc16@gmail.com>
2025-07-24 21:52:06 -0500

    Update StandardDeck.java

--- StandardDeck.java
+++ StandardDeck.java
@@ -1,69 +1,55 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 
 public class StandardDeck {
-	private ArrayList<StandardCard> deck = new ArrayList<StandardCard>();
-	
+	private ArrayList<StandardCard> deck = new ArrayList<>();
+
 	public StandardDeck() {
 		reset();
 		shuffleDeck();
 	}
-	
+
 	public void reset() {
 		this.deck.clear();
-		// Adding in hearts suit
-		for (int i = 2; i < 15; i++) {
-			deck.add(new StandardCard(i,"Hearts"));
-		}
-		
-		// Adding in diamonds suit
-		for (int j = 2; j < 15; j++) {
-			deck.add(new StandardCard(j,"Diamonds"));
-		}
-		
-		// Adding in spades suit
-		for (int k = 2; k < 15; k++) {
-			deck.add(new StandardCard(k,"Spades"));
-		}
-		
-		// Adding in clubs suit
-		for (int l = 2; l < 15; l++) {
-			deck.add(new StandardCard(l,"Clubs"));
+		String[] suits = {"Hearts", "Diamonds", "Spades", "Clubs"};
+		for (String suit : suits) {
+			for (int i = 2; i <= 14; i++) {
+				deck.add(new StandardCard(i, suit));
+			}
 		}
 	}
-	
+
 	public void shuffleDeck() {
-		ArrayList<StandardCard> tempDeck = new ArrayList<StandardCard>();
-		while (this.deck.size() > 0) {
-			int randomIndex = ((int) (Math.random() * 100)) % this.deck.size();
-			tempDeck.add(this.deck.remove(randomIndex));
-		}
-		this.deck = tempDeck;
+		Collections.shuffle(this.deck);  // Uses built-in shuffle
 	}
-	
+
 	public StandardCard getNextCard() {
-		return this.deck.remove(this.deck.size()-1);
+		if (this.deck.isEmpty()) {
+			return null;
+		}
+		return this.deck.remove(this.deck.size() - 1);
 	}
-	
+
 	public int getRemainingCardCount() {
 		return this.deck.size();
 	}
-	
-	public static void main(String[] args) {
-		// List of StandardCard's - Done
-		// reset (clears and adds new list of cards) - Done
-		// shuffle deck - Done
-		// get next card - Done
-		// get remaining number of cards - Done
-		
-		StandardDeck sd = new StandardDeck();
-		System.out.println(sd.deck.toString());
-		System.out.println(sd.getNextCard());
-		System.out.println(sd.getNextCard());
-		System.out.println(sd.getNextCard());
-		System.out.println(sd.getNextCard());
-		System.out.println(sd.getRemainingCardCount());
 
+	// Optional: display deck without exposing private field
+	public String showDeck() {
+		return this.deck.toString();
 	}
 
+	public static void main(String[] args) {
+		StandardDeck sd = new StandardDeck();
+		System.out.println("Full shuffled deck:");
+		System.out.println(sd.showDeck());
+
+		System.out.println("\nDrawing 4 cards:");
+		System.out.println(sd.getNextCard());
+		System.out.println(sd.getNextCard());
+		System.out.println(sd.getNextCard());
+		System.out.println(sd.getNextCard());
+
+		System.out.println("\nRemaining cards in deck: " + sd.getRemainingCardCount());
+	}
 }
